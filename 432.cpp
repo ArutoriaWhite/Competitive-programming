7dd96abc414033d8a0fd1d0657f1d8cb5fb70a7f
ArutoriaWhite <52879291+ArutoriaWhite@users.noreply.github.com>
2019-08-01 22:53:24 +0800

    Update 432.cpp

--- 432.cpp
+++ 432.cpp
@@ -1,5 +1,7 @@
 #define _for(i,a,b) for(int i=(a);i<(b);i++)
-#define vec pair<int,int>
+#define _vec pair<int,int>
+#define _at(p) [p.second][p.first]
+#define _vecPlus(v,x,y) (_vec(v.first+x,v.second+y))
 #include <tuple>
 #include <set>
 #include <map>
@@ -7,56 +9,49 @@
 #include <iostream>
 #include <algorithm>
 #include <numeric>
+#include <stack>
 #include <queue>
 using namespace std;
 
-bool G[1000][1000];
+int G[1002][1002];
 int N, M;
-bool isOut=false;
-vec me;
-vec target;
+bool canOut = false;
+stack<_vec> q;
+_vec me;
+_vec target;
 
-void BFS(vec u) {
-	queue<vec> q;
-	q.push(u);
-
-	while (q.size()>0){
-		vec v = q.front(); q.pop();
-		if (v == target) {
-			isOut = true;
-			return;
-		}
-		if (!G[v.second + 1][v.first]) q.push(vec(v.second + 1, v.first));
-		if (!G[v.second][v.first + 1]) q.push(vec(v.second, v.first + 1));
-		if (!G[v.second - 1][v.first]) q.push(vec(v.second - 1, v.first));
-		if (!G[v.second][v.first - 1]) q.push(vec(v.second, v.first - 1));
-		G[v.second][v.first] = true;
-	}
-
+void DFS(_vec u) {
+	G _at(u) = true;
+	if (u == target) canOut = true;
+	if (!G _at(_vecPlus(u, 1, 0))) DFS(_vecPlus(u, 1, 0));
+	if (!G _at(_vecPlus(u, -1, 0))) DFS(_vecPlus(u, -1, 0));
+	if (!G _at(_vecPlus(u, 0, 1))) DFS(_vecPlus(u, 0, 1));
+	if (!G _at(_vecPlus(u, 0, -1))) DFS(_vecPlus(u, 0, -1));
 }
 
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-
 	cin >> N >> M;
-	_for(y, 1, M+1) {
-		_for(x, 1, N+1) {
+	_for(y, 0, M + 2) G[y][0] = G[y][N + 1] = true;
+	_for(x, 0, N + 2) G[0][x] = G[M + 1][x] = true;
+	_for(y, 1, M + 1) {
+		_for(x, 1, N + 1) {
 			G[y][x] = false;
 		}
 	}
-	int Xme, Yme; cin >> Xme >> Yme;
-	me = vec(Xme, Yme);
+	int x, y; cin >> x >> y;
+	me = _vec(x, y);
 	int xt, yt; cin >> xt >> yt;
-	target=vec(xt,yt);
+	target = _vec(xt, yt);
 
 	int f; cin >> f;
 	_for(i, 0, f) {
 		int x, y; cin >> x >> y;
 		G[y][x] = true;
 	}
-	BFS(me);
-	if (isOut) cout << "Cool!\n";
+	DFS(me);
+	if (canOut) cout << "Cool!\n";
 	else cout << "Harakiri!\n";
 	return 0;
 }
