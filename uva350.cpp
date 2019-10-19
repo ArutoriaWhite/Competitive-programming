ecdced0fe0b4b8ecd53dd8157a54d0458cc6ec38
ArutoriaWhite <aatroxvanz@gmail.com>
2019-10-19 19:28:13 +0800

    ria

--- uva350.cpp
+++ uva350.cpp
@@ -3,7 +3,7 @@ using namespace std;
 #define maxM 10010
 
 int z,i,m,l,cnt;
-int used[maxM], u=0;
+short used[maxM], time[maxM], u=0;
 
 int main()
 {
@@ -14,7 +14,7 @@ int main()
     {
         if (z==i && i==m && m==l && l==0) break;
         cout << "Case " << ++u;
-        for( cnt=0; used[l]<u; used[l]=u,l=((z*l+i)%m),cnt++);
-        cout << ": " << cnt << '\n';
+        for( cnt=0; used[l]<u; used[l]=u,time[l]=cnt,l=((z*l+i)%m),cnt++);
+        cout << ": " << cnt-time[l] << '\n';
     }
 }
