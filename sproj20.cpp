65f87c44499feb340113a0fd3ec7dc7a04e59459
arutoria <aatroxvanz@gmail.com>
2019-11-15 01:08:18 +0800

    non

---
+++ sproj20.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+
+int bigq[maxN], bigf, bigr;
+int smalq[maxN][maxN], q[maxN], r[maxN];
+
--- uva355.cpp
+++ uva355.cpp
@@ -7,38 +7,58 @@ inline int ctoi( char c)
 	if ('A'<=c&&c<='Z') return (c-'A'+10);
 	else return (c-'0');
 }
+inline char itoc( int n)
+{
+	if (0<=n&&n<=9) return '0'+n;
+	else return 'A'+n;
+}
 
 inline ll stoi( string s, int n)
 {
 	ll res = 0;
-	for (int i=0; i<s.size(); i++,res+=ctoi(s[i]))
-		res *= n;
+	for (int i=0; i<s.size(); i++)
+		res *= n, res += ctoi(s[i]);
 	return res;
 }
 
-inline char itos( int x, int n)
+string res;
+inline void itos( int x, int n)
 {
-	char s[100];
-	int j;
-	for (j=100; x>0; x/n)
+	res = "";
+	if (x==0) res = "0";
+	else
 	{
-		s[--j] = x%n;
-		x/n;
+		while (x>0)
+		{
+			res += itoc(x%n);
+			x/=n;
+		}
 	}
-	return &(s[j]+1);
 }
 
 int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
 	int n, m;
 	string s;
+
 	while (cin >> m >> n >> s)
 	{
 		int k=2;
 		for (int i=0; i<s.size(); i++)
 			k = max(k,ctoi(s[i])+1);
-		if (k>n || k>m)
+		if (k>m)
 			cout << s << " is an illegal base " << m << " number\n";
-		else cout << 
+		else
+		{
+			itos (stoi(s,m), n);
+			cout << s<< " base " << m << " = ";
+			for (int i=res.size()-1; i>=0; i--)
+				cout << res[i];
+			cout << " base " << n << "\n";
+		}
+	
 	}
 	return 0;
 }
