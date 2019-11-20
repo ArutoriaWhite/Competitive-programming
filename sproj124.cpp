bcb1000d12ac8e0d15b535f3765c0433ecf34524
arutoria <aatroxvanz@gmail.com>
2019-11-20 10:58:04 +0800

    non

--- sproj124.cpp
+++ sproj124.cpp
@@ -1,4 +1,11 @@
-// square len, base x, base y, filled x, filled y
+#define M1 mx-1,my
+#define M2 mx-1,my-1
+#define M3 mx,my-1
+#define M4 mx,my
+#define B1 bx,my
+#define B2 bx,by
+#define B3 mx,by
+#define B4 mx,my
 void msol (int n, int bx, int by, int fx, int fy)
 {
 	if (n==1) return;
@@ -6,35 +13,35 @@ void msol (int n, int bx, int by, int fx, int fy)
 	int mx = bx+(n>>1), my = by+(n>>1);
 	if (fx>=mx && fy>=my) // 4
 	{
-		Report(mx-1,my,mx-1,my-1,mx,my-1);
-		msol(n>>1,bx,my,mx-1,my);
-		msol(n>>1,bx,by,mx-1,my-1);
-		msol(n>>1,mx,by,mx,my-1);
-		msol(n>>1,mx,my,fx,fy);
+		Report(M1,M2,M3);
+		msol(n>>1,B1,M1);
+		msol(n>>1,B2,M2);
+		msol(n>>1,B3,M3);
+		msol(n>>1,B4,fx,fy);
 	}
 	else if (fx>=mx && fy<my) //3
 	{
-		Report(mx-1,my,mx-1,my-1,mx,my);
-		msol(n>>1,bx,my,mx-1,my);
-		msol(n>>1,bx,by,mx-1,my-1);
-		msol(n>>1,mx,my,mx,my);
-		msol(n>>1,mx,by,fx,fy);
+		Report(M1,M2,M4);
+		msol(n>>1,B1,M1);
+		msol(n>>1,B2,M2);
+		msol(n>>1,B4,M4);
+		msol(n>>1,B3,fx,fy);
 	}
 	else if (fx<mx && fy>=my) // 1
 	{
-		Report(mx-1,my-1,mx,my-1,mx,my);
-		msol(n>>1,bx,by,mx-1,my-1);
-		msol(n>>1,mx,by,mx,my-1);
-		msol(n>>1,mx,my,mx,my);
-		msol(n>>1,bx,my,fx,fy);
-	}
+		Report(M2,M3,M4);
+		msol(n>>1,B2,M2);
+		msol(n>>1,B3,M3);
+		msol(n>>1,B4,M4);
+		msol(n>>1,B1,fx,fy);
+}
 	else if (fx<mx && fy<my) // 2
 	{
-		Report(mx-1,my,mx,my-1,mx,my);
-		msol(n>>1,bx,my,mx-1,my);
-		msol(n>>1,mx,by,mx,my-1);
-		msol(n>>1,mx,my,mx,my);
-		msol(n>>1,bx,by,mx-1,my-1);
+		Report(M1,M3,M4);
+		msol(n>>1,B1,M1);
+		msol(n>>1,B3,M3);
+		msol(n>>1,B4,M4);
+		msol(n>>1,B2,fx,fy);
 	}
 }
 
@@ -43,4 +50,3 @@ void solve (int n, int x, int y)
 {
 	msol(n,1,1,x,y);
 }
-
