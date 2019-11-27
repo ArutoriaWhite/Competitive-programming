c35afb84d6331df09c10665bca0b0594f5237ef1
arutoria <aatroxvanz@gmail.com>
2019-11-27 23:38:11 +0800

    non

--- toj43.cpp
+++ toj43.cpp
@@ -1,48 +1,60 @@
 #include<iostream>
 #include<vector>
-#include<queue>
+#include<tuple>
 #include<string.h>
+#include<queue>
 using namespace std;
-#define maxN 10005
-#define rep(i,a,b) for(int i=(a) ; i<(b) ; i++ )
+#define maxN (10000+10)
 
-typedef pair<int,int> pii;
+typedef pair<int,int> edge;
+#define W first
+#define X second
 
-vector<pii> edges[maxN];
+typedef pair<int,int> ele;
+#define D first
+#define X second
+
+vector<edge> gra[maxN];
+
 int dis[maxN];
+priority_queue<ele> pq;
 
-void dijks( int rt ){
-    memset( dis, 0x3f3f3f3f, sizeof(dis)); dis[rt] = 0;
-    priority_queue< pii, vector<pii>, greater<pii> > pq;
-    pq.push( pii(0,rt));
+void dijks( int rt)
+{
+	memset(dis,0x3f,sizeof(dis));
+	pq.push( ele(0,rt) );
 
-    while( !pq.empty()){
-        int  u = pq.top().second, d = pq.top().first;
-        pq.pop();
-        if( dis[u] < d) continue;
+	while (!pq.empty())
+	{
+		ele u = pq.top();
+		pq.pop();
 
-        for( auto v: edges[u]){
-            if( dis[v.second] > d+v.first){
-                dis[v.second] = d+v.first;
-                pq.push( make_pair( dis[v.second], v.second));
-            }
-        }
-    }
+		if (dis[u.X] < u.D) continue;
+		for (auto v: gra[u.X])
+		{
+			if (dis[v.X] > u.D+v.W) 
+			{
+				dis[v.X] = u.D+v.W;
+				pq.push( make_pair( dis[v.X], v.X ) );
+			}
+		}
+	}
 }
 
-int main()
+int main ()
 {
 	int n, m;
 	cin >> n >> m;
 	for (int i=0; i<m; i++)
 	{
-		int a, b, L;
-		cin >> a >> b >> L;
-		edges[a].push_back(make_pair(L,b));
-		edges[b].push_back(make_pair(L,a));
+		int a, b, c;
+		cin >> a >> b >> c;
+		gra[a].push_back( make_pair(c,b) );
+		gra[b].push_back( make_pair(c,a) );
 	}
-	int s, e;
-	cin >> s >> e;
-	dijks(s);
-	cout << dis[e] << '\n';
+
+	int oao, tat;
+	cin >> oao >> tat;
+	dijks(oao);
+	cout << dis[tat] << '\n';
 }
