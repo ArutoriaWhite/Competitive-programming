1d71b9849d92eabba7181d182111fd7d0976da4c
ArutoriaWhite <52879291+ArutoriaWhite@users.noreply.github.com>
2019-12-19 20:18:36 +0800

    Update liu_she_kao_hao_nan_pf.cpp

--- liu_she_kao_hao_nan_pf.cpp
+++ liu_she_kao_hao_nan_pf.cpp
@@ -1,11 +1,12 @@
 #include<iostream>
+#include<unordered_set>
 using namespace std;
 const int N = 3000+10, M = 1e8;
 
 int tow[][2] = { {1,0}, {-1,0}, {0,1}, {0,-1} };
 int res[N], n, m, k;
 bool used[N][N];
-int vis[N][N][4];
+unordered_set<int> vis[N][N];
 int qx[M], qy[M], qp[M], f, r;
 
 inline bool out (int x, int y)
@@ -21,20 +22,13 @@ inline void bfs()
 		used[x][y] = 1;
 		res[p]++;
 		
-		for (int i=0; i<4; i++)
+		for (int j=0; j<4; j++)
 		{
-			int vx = x+tow[i][0], vy = y+tow[i][1];
+			int vx = x+tow[j][0], vy = y+tow[j][1];
 			if (out(vx,vy) || used[vx][vy]) continue;
-			for (int i=0; i<4; i++)
-			{
-				if (vis[vx][vy][i] == p) break;
-				if (vis[vx][vy][i]==0)
-				{
-					vis[vx][vy][i] = p;
-					qx[r] = vx, qy[r] = vy, qp[r] = p; r++;
-					break;
-				}
-			}
+			if (vis[vx][vy].count(p)) continue;
+			vis[vx][vy].insert(p);
+			qx[r] = vx, qy[r] = vy, qp[r] = p, r++;
 		}
 	}
 }
@@ -48,7 +42,6 @@ signed main()
 	cin >> n >> m >> k;
 	for (int i=1; i<=k; i++)
 	{
-		int x, y;
 		cin >> qx[r] >> qy[r];
 		qp[r] = i;
 		r++;
