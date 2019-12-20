f2e9b4391b0ee793bfa2a7bc5f0e4e724574e1f7
ArutoriaWhite <52879291+ArutoriaWhite@users.noreply.github.com>
2019-12-20 10:26:42 +0800

    Update liu_she_kao_hao_nan_pf.cpp

--- liu_she_kao_hao_nan_pf.cpp
+++ liu_she_kao_hao_nan_pf.cpp
@@ -1,49 +1,45 @@
 #include<iostream>
-#include<unordered_set>
 using namespace std;
-const int N = 3000+10, M = 1e8;
+const int N = 3e3+10, M = 1e8;
 
-int tow[][2] = { {1,0}, {-1,0}, {0,1}, {0,-1} };
-int res[N], n, m, k;
-bool used[N][N];
-unordered_set<int> vis[N][N];
+int n, m, k, res[N];
+int dep[N][N], las[N][N], tow[][2] = { {1,0}, {0,1}, {-1,0}, {0,-1} };
 int qx[M], qy[M], qp[M], f, r;
 
-inline bool out (int x, int y)
+inline bool out (int x,int y)
 {
 	return x<=0 || y<=0 || x>n || y>m;
-}
+}	
 
 inline void bfs()
 {
 	while (f<r)
 	{
-		int x = qx[f], y=qy[f], p=qp[f]; f++;
-		used[x][y] = 1;
+		int x=qx[f], y=qy[f], p=qp[f], d=dep[x][y]; f++;
 		res[p]++;
 		
-		for (int j=0; j<4; j++)
+		for (int i=0; i<4; i++)
 		{
-			int vx = x+tow[j][0], vy = y+tow[j][1];
-			if (out(vx,vy) || used[vx][vy]) continue;
-			if (vis[vx][vy].count(p)) continue;
-			vis[vx][vy].insert(p);
-			qx[r] = vx, qy[r] = vy, qp[r] = p, r++;
+			int vx=x+tow[i][0], vy=y+tow[i][1], vd=d+1;
+			if (!out(vx,vy) && (dep[vx][vy]==0 || (dep[vx][vy]==vd&&las[vx][vy]!=p)))
+			{
+				dep[vx][vy] = vd;
+				las[vx][vy] = p;
+				qx[r] = vx, qy[r] = vy, qp[r] = p, r++;		
+			}
 		}
 	}
 }
 
-signed main()
+int main()
 {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	
 	f=r=0;
 	cin >> n >> m >> k;
 	for (int i=1; i<=k; i++)
 	{
 		cin >> qx[r] >> qy[r];
 		qp[r] = i;
+		dep [qx[r]][qy[r]] = 1;
 		r++;
 	}
 	
