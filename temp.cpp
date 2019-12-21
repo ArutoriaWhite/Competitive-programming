3bdaaf8fd670d446ce0651879cca9d223039ac42
Arutoria <aatroxvanz@gmail.com>
2019-12-22 00:20:35 +0800

    non

--- temp.cpp
+++ temp.cpp
@@ -1,11 +1,9 @@
 #include<iostream>
-#include<cstring>
 using namespace std;
 
-int ouo[3];
 int main()
 {
-	memset(ouo,0x7f,sizeof(ouo));
-	for (int i=0; i<3; i++)
-		cout << ouo[i] << ' ';
+	int n;
+	cin >> n;
+	cout << n+1 << '\n';
 }
