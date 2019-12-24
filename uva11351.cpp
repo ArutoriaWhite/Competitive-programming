1b9b550ec88219cb5a4cbbc0b3fa7a4375644202
ArutoriaWhite <52879291+ArutoriaWhite@users.noreply.github.com>
2019-12-24 18:54:44 +0800

    Update uva11351.cpp

--- uva11351.cpp
+++ uva11351.cpp
@@ -3,18 +3,18 @@ using namespace std;
 
 int f (int n, int k)
 {
-    if (n<=k) return (k-1)%n;
-    else return (f(n-1,k)+k)%n;
+	if (n==1) return 0;
+	else return ((k-1)%n+f(n-1,k)+1)%n;
 }
 
 int main()
 {
-    int T, t=0;
-    cin >> T;
-    while (t<T)
-    {
-        int n, k;
-        cin >> n >> k;
-        cout << "Case " << ++t << ": " << f(n,k)     << "\n";
-    }
+	int T;
+	cin >> T;
+	for (int i=1; i<=T; i++)
+	{
+		int n, k;
+		cin >> n >> k;
+		cout << "Case " << i << ": " << f(n,k)+1 << "\n";
+	}
 }
