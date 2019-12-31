b967e3fc2950d9d1f37a7491a8bd034f748e4124
Arutoria <aatroxvanz@gmail.com>
2019-12-31 21:06:43 +0800

    non

--- sproj42.cpp
+++ sproj42.cpp
@@ -1,54 +1,54 @@
 #include<iostream>
 using namespace std;
-const int N=1e3+10;
+const int N=1e3+10, M=1e8;
 
 string G[N];
-int mX, mY;
 int wal[4][2] = { {1,0}, {-1,0}, {0,1}, {0,-1} };
-int qx[M], qy[M], f, r, vis[N][N], t;
+int vis[N][N], t, mX, mY;
+int qx[M], qy[M], f, r;
 
 inline bool can (int x, int y)
 {
-    return 0<=x&&x<mX && 0<=y&&y<mY && vis[vy][vx]<t && G[vx][vy]=='.';
+    return 0<=x&&x<mX && 0<=y&&y<mY && vis[y][x]<t && G[y][x]!='#';
 }
 
-inline void bfs (int rtx, int rty)
+void bfs (int rx, int ry)
 {
-    qx[r] = rtx, qy[r] = rty, r++;
+    f=r=0;
+    qx[r]=rx, qy[r]=ry, r++;
     while (f<r)
     {
-        int ux=qx[f], uy=qy[f]; f++;
         for (int i=0; i<4; i++)
         {
-            int vx=ux+walk[i][0], vy=uy+walk[i][1];
-            if (!can(vx,vy)) continue;
-            qx[r]=vx, qy[r]=vy, r++;
+            int vx=qx[f]+wal[i][0], vy=qy[f]+wal[i][1];
+            if (can(vx,vy))
+            {
+                vis[vy][vx]=t;
+                qx[r]=vx, qy[r]=vy, r++;
+            }
         }
+        f++;
     }
 }
 
 int main()
 {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
     int T;
     cin >> T;
     for (t=1; t<=T; t++)
     {
-        f=r=0;
         cin >> mY >> mX;
         for (int y=0; y<mY; y++)
             cin >> G[y];
 
         int res=0;
         for (int y=0; y<mY; y++)
-        {
             for (int x=0; x<mX; x++)
-            {
-                if (vis[y][x]>=t) continue;
-                bfs (x,y);
-                res++;
-            }
-        }
+                if (can(x,y))
+                    res++, bfs(x,y);
         cout << res << '\n';
     }
 }
-
