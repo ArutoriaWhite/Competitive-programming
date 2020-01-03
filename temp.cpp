05f32056efa4500be77ea2d227743e9ee41ad372
Arutoria <aatroxvanz@gmail.com>
2020-01-04 01:33:44 +0800

    non

--- temp.cpp
+++ temp.cpp
@@ -1,6 +1,27 @@
-#include<instorm>
+#include<iostream>
+#include<algorithm>
+#include<vector>
 using namespace std;
-int a;
-cin>>a;
-a=a*a;
-cout<<a;
+
+int v[100];
+
+int main()
+{
+    int n, x;
+    cin >> n;
+    for (int i=0; i<n; i++)
+        cin >> v[i];
+
+    cin >> x;
+    *(lower_bound(v,v+n,x)-1)=1;
+    for (int i=0; i<n; i++)
+        cout << v[i] << ' ';
+    cout << '\n';
+}
+
+// vector
+// x<s[0], lwb(x) = s.begin();
+// s.back()<x, lwb(x) = s.end();
+
+// array
+// x<s[0], 
