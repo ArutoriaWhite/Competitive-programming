5a837bcac6a11bfb6c8b95f0ce5a2869b4d42eac
Arutoria <aatroxvanz@gmail.com>
2020-01-24 21:50:31 +0800

    non

--- sproj62.cpp
+++ sproj62.cpp
@@ -1,10 +1,9 @@
-#include <iostream>
-#include <memory.h>
+#include <bits/stdc++.h>
 using namespace std;
 const int N = 9;
 
 int hor[N], ver[N], blo[3][3], sol[N*N];
-string map;
+string mp;
 
 inline void init()
 {
@@ -14,26 +13,17 @@ inline void init()
 }
 inline void add (int x, int y, int k)
 {
-    hor[y] |= (1<<k);
-    ver[x] |= (1<<k);
-    blo[x/3][y/3] |= (1<<k);
+    hor[y] |= (1<<k), ver[x] |= (1<<k), blo[x/3][y/3] |= (1<<k);
 }
 inline void del (int x, int y, int k)
 {
-    hor[y] &= ~(1<<k);
-    ver[x] &= ~(1<<k);
-    blo[x/3][y/3] &= ~(1<<k);
+    hor[y] &= ~(1<<k), ver[x] &= ~(1<<k), blo[x/3][y/3] &= ~(1<<k);
 }
 inline void build ()
 {
-    for (int i=0,len=map.size(),x,y; i<len; i++)
-    {
-        if (map[i]!='.')
-        {
-            x = i%N, y = i/N;
-            add(x,y,map[i]-'0');
-        }
-    }
+    for (int i=0,len=mp.size(); i<len; i++)
+        if (mp[i]!='.')
+            add(i%N,i/N,mp[i]-'0');
 }
 inline bool can (int x, int y, int k)
 {
@@ -44,14 +34,13 @@ bool dfs (int d)
     int y=d/N, x=d%N;
     if (d>=N*N)
     {
-        for (int i=0; i<N*N; i++)
-            cout << sol[i];
+        for (int i=0; i<N*N; i++) cout << sol[i];
         cout << '\n';
         return 1;
     }
-    if (map[d]!='.')
+    if (mp[d]!='.')
     {
-        sol[d] = map[d]-'0';
+        sol[d] = mp[d]-'0';
         return dfs(d+1);
     }
     for (int i=1; i<=9; i++)
@@ -65,10 +54,9 @@ bool dfs (int d)
 }
 int main()
 {
-    while (cin >> map && map!="end")
+    while (cin >> mp && mp!="end")
     {
-        init();
-        build();
+        init(), build();
         if (!dfs(0)) cout << "No solution.\n";
     }
 }
